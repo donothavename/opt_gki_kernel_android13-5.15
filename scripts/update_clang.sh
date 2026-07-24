e840455c26491f67febe6271814e0935ad0bea11
donothavename <2530648358@qq.com>
2026-07-24 21:17:07 +0800

    suppress new clang const-init warnings

--- scripts/update_clang.sh
+++ scripts/update_clang.sh
@@ -2,9 +2,11 @@ update_clang() {
     local common_dir clang_ver
     common_dir="$(abk_common_dir)"
 
+    abk_require_file "$common_dir/Makefile"
     abk_require_file "$common_dir/build.config.constants"
 
     sed -i 's/CLANG_VERSION=.*/CLANG_VERSION=r584948c/' "$common_dir/build.config.constants"
+    sed -i 's/		   -std=gnu89/		   -std=gnu89 \\\n		   -Wno-default-const-init-var-unsafe \\\n		   -Wno-default-const-init-field-unsafe \\\n		   -Wno-uninitialized-const-pointer/' "$common_dir/Makefile"
 
     abk_log "更新 clang，拉取 main-kernel-2026 分支"
 
