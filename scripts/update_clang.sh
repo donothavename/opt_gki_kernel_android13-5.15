43d10a602078b774d189bbdf9441605edc3d87ac
donothavename <2530648358@qq.com>
2026-09-14 06:31:42 +0800

    update

--- scripts/update_clang.sh
+++ scripts/update_clang.sh
@@ -5,12 +5,16 @@ update_clang() {
     abk_require_file "$common_dir/Makefile"
     abk_require_file "$common_dir/build.config.constants"
 
-    sed -i 's/CLANG_VERSION=.*/CLANG_VERSION=r596125/' "$common_dir/build.config.constants"
+    sed -i 's/CLANG_VERSION=.*/CLANG_VERSION=r614150/' "$common_dir/build.config.constants"
     sed -i 's/\t\t   -std=gnu89/\t\t   -std=gnu89 \\\n\t\t   -Wno-default-const-init-var-unsafe \\\n\t\t   -Wno-default-const-init-field-unsafe \\\n\t\t   -Wno-uninitialized-const-pointer/' "$common_dir/Makefile"
 
-    abk_log "更新 clang，拉取 main-kernel-2026 分支"
+    abk_log "更新 clang，拉取 main-kernel 分支"
 
     cd "$KERNEL_ROOT/prebuilts/clang/host"
     rm -r linux-x86/
-    git clone -b main-kernel-2026 --single-branch https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 --depth=1
+    git clone --filter=blob:none --no-checkout --depth=1  -b main-kernel https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86
+    cd linux-x86
+    git sparse-checkout init --no-cone
+    echo '/*\n!/clang-*/\n/clang-r614150/' > ".git/info/sparse-checkout"
+    git checkout
 }
